cdbdb0b122c3de8c6fa69a4eba9af149575bb535
费陶勇 <2460392754@qq.com>
2022-03-01 19:32:27 +0800

    feat: 文档首页添加友情链接

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -2,22 +2,7 @@
     <ParentLayout>
         <template #page-bottom>
             <Modal />
-            <div class="footer">
-                <span>友情链接: </span>
-
-                <a
-                    v-for="(item, i) of LinksData"
-                    class="external-link"
-                    rel="noopener noreferrer"
-                    target="_blank"
-                    :href="item.like"
-                    :aria-label="item.text"
-                >
-                    <span class="name">{{ item.text }}</span>
-                    <ExternalLinkIcon />
-                </a>
-                <div class="copyright">Copyright © 2022 | 浙ICP备19047959号-3</div>
-            </div>
+            <Footer />
         </template>
     </ParentLayout>
 </template>
@@ -25,22 +10,14 @@
 <script lang="ts">
 import ParentLayout from 'vuepress-theme-quicksand/lib/layouts/Layout.vue';
 import Modal from '../../../components/modal.vue';
-import { useRouteLocale } from '@vuepress/client';
-import { ExternalLinkIcon } from '@vuepress/plugin-external-link-icon/lib/client/index.js';
-import LinksData from '../../data/links';
+import Footer from '../../../components/footer.vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
     components: {
-        ParentLayout,
-        Modal,
-        ExternalLinkIcon
-    },
-
-    setup() {
-        return {
-            LinksData
-        };
+        ParentLayout
+        // Modal,
+        // Footer
     }
 });
 </script>
---
+++ docs/components/footer.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="footer">
+        <span>友情链接: </span>
+
+        <a
+            v-for="(item, i) of LinksData"
+            class="external-link"
+            rel="noopener noreferrer"
+            target="_blank"
+            :href="item.link"
+            :aria-label="item.text"
+        >
+            <span class="name">{{ item.text }}</span>
+            <ExternalLinkIcon />
+        </a>
+        <div class="copyright">Copyright © 2022 | 浙ICP备19047959号-3</div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { ExternalLinkIcon } from '@vuepress/plugin-external-link-icon/lib/client/index.js';
+import LinksData from '../.vuepress/data/links';
+
+export default defineComponent({
+    components: {
+        ExternalLinkIcon
+    },
+
+    setup() {
+        return {
+            LinksData
+        };
+    }
+});
+</script>
+
+<style lang="scss" scoped>
+.footer {
+    text-align: center;
+
+    .external-link {
+        color: var(--c-text-accent);
+        margin-left: 10px;
+    }
+
+    .copyright {
+        text-align: center;
+        margin-top: 20px;
+    }
+}
+</style>
