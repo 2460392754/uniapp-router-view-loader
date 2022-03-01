615de54b96fa7e66e5d92161c7510092266df700
费陶勇 <2460392754@qq.com>
2022-03-02 01:18:16 +0800

    feat: 本地测试脚本

---
+++ scripts/localTest.js
@@ -0,0 +1,31 @@
+/*
+ * @Author: 2460392754@qq.com
+ * @Date: 2022-03-02 00:43:54
+ * @LastEditTime: 2022-03-02 01:17:59
+ * @Description: 本地部署，替换 `examples` 里各项目的 `node_modules/**\/dist` 文件内容
+ * @FilePath: /uniapp-router-view-loader/scripts/localTest.js
+ */
+
+const fs = require('fs');
+const fse = require('fs-extra');
+const path = require('path')
+const { name } = require('../package.json')
+const chalk = require("chalk");
+
+const dirPath = './examples'
+const dirChildrenPathAll = fs.readdirSync(dirPath)
+
+console.log(chalk.green(`Tip: 本地部署开始`));
+
+for (const childPath of dirChildrenPathAll) {
+    const { name } = require('../package.json')
+    const toDir = './' + path.join(dirPath, childPath, 'node_modules', name, 'dist')
+
+    fse.copy('./dist', toDir, (err) => {
+        if (err) throw err;
+
+        console.log(chalk.yellow(`Tip: 本地部署成功【${toDir}】`));
+    });
+}
+
+console.log(chalk.green(`Tip: 本地部署结束`));
