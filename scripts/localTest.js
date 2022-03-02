8c2ac91e04f38f2b6ff5f8255239df5e0dedf0d2
费陶勇 <2460392754@qq.com>
2022-03-02 11:09:55 +0800

    fix：修改  localTest.js 教程同步化

--- scripts/localTest.js
+++ scripts/localTest.js
@@ -1,7 +1,7 @@
 /*
  * @Author: 2460392754@qq.com
  * @Date: 2022-03-02 00:43:54
- * @LastEditTime: 2022-03-02 01:17:59
+ * @LastEditTime: 2022-03-02 11:04:30
  * @Description: 本地部署，替换 `examples` 里各项目的 `node_modules/**\/dist` 文件内容
  * @FilePath: /uniapp-router-view-loader/scripts/localTest.js
  */
@@ -21,11 +21,12 @@ for (const childPath of dirChildrenPathAll) {
     const { name } = require('../package.json')
     const toDir = './' + path.join(dirPath, childPath, 'node_modules', name, 'dist')
 
-    fse.copy('./dist', toDir, (err) => {
-        if (err) throw err;
-
+    try {
+        fse.copySync('./dist', toDir)
         console.log(chalk.yellow(`Tip: 本地部署成功【${toDir}】`));
-    });
+    } catch (err) {
+        console.error(err)
+    }
 }
 
 console.log(chalk.green(`Tip: 本地部署结束`));
