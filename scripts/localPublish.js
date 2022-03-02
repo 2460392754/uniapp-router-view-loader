efc668d2f37d2e685613b610d3279cfcf22f8d2b
费陶勇 <2460392754@qq.com>
2022-03-02 11:43:22 +0800

    feat：添加 examples项目版本号同步的插件

--- scripts/localPublish.js
+++ scripts/localPublish.js
@@ -1,8 +1,34 @@
 /*
  * @Author: 2460392754@qq.com
  * @Date: 2022-03-02 00:43:54
- * @LastEditTime: 2022-03-02 01:17:52
- * @Description: 
+ * @LastEditTime: 2022-03-02 11:42:38
+ * @Description: 测试文件同步插件版本号
  * @FilePath: /uniapp-router-view-loader/scripts/localPublish.js
  */
 
+const fs = require('fs');
+const path = require('path')
+const chalk = require("chalk");
+const { name, version } = require('../package.json')
+
+const dirPath = './examples'
+const dirChildrenPathAll = fs.readdirSync(dirPath)
+
+console.log(chalk.green(`Tip: 测试项目本地部署版本号开始`));
+
+// 修改版本号, 并锁定
+for (const childPath of dirChildrenPathAll) {
+    const toPackageJsonPath = './' + path.join(dirPath, childPath, 'package.json')
+    const content = JSON.parse(fs.readFileSync(toPackageJsonPath, 'utf8'));
+
+    content.devDependencies[name] = version;
+
+    try {
+        fs.writeFileSync(toPackageJsonPath, JSON.stringify(content, null, 4), 'utf8')
+        console.log(chalk.yellow(`Tip: 测试项目本地部署版本号成功【${toPackageJsonPath}】`));
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+console.log(chalk.green(`Tip: 测试项目本地部署版本号结束`));
--- scripts/localTest.js
+++ scripts/localTest.js
@@ -1,32 +1,31 @@
 /*
  * @Author: 2460392754@qq.com
  * @Date: 2022-03-02 00:43:54
- * @LastEditTime: 2022-03-02 11:04:30
- * @Description: 本地部署，替换 `examples` 里各项目的 `node_modules/**\/dist` 文件内容
+ * @LastEditTime: 2022-03-02 11:41:28
+ * @Description: 本地部署测试文件，替换 `examples` 里各项目的 `node_modules/**\/dist` 文件内容
  * @FilePath: /uniapp-router-view-loader/scripts/localTest.js
  */
 
 const fs = require('fs');
 const fse = require('fs-extra');
 const path = require('path')
-const { name } = require('../package.json')
 const chalk = require("chalk");
+const { name } = require('../package.json')
 
 const dirPath = './examples'
 const dirChildrenPathAll = fs.readdirSync(dirPath)
 
-console.log(chalk.green(`Tip: 本地部署开始`));
+console.log(chalk.green(`Tip: 测试项目本地部署测试文件开始`));
 
 for (const childPath of dirChildrenPathAll) {
-    const { name } = require('../package.json')
     const toDir = './' + path.join(dirPath, childPath, 'node_modules', name, 'dist')
 
     try {
         fse.copySync('./dist', toDir)
-        console.log(chalk.yellow(`Tip: 本地部署成功【${toDir}】`));
+        console.log(chalk.yellow(`Tip: 测试项目本地部署成功【${toDir}】`));
     } catch (err) {
         console.error(err)
     }
 }
 
-console.log(chalk.green(`Tip: 本地部署结束`));
+console.log(chalk.green(`Tip: 测试项目本地部署测试文件结束`));
