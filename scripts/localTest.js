054b1c4c07244728e1bb1aff588e1b4691f1c342
费陶勇 <2460392754@qq.com>
2022-03-04 18:11:48 +0800

    script: 修复本地部署脚本 路径判断未判断内容类型导致部署异常

--- scripts/localTest.js
+++ scripts/localTest.js
@@ -1,7 +1,7 @@
 /*
  * @Author: 2460392754@qq.com
  * @Date: 2022-03-02 00:43:54
- * @LastEditTime: 2022-03-03 15:58:57
+ * @LastEditTime: 2022-03-04 16:43:14
  * @Description: 本地部署测试文件，替换 `examples` 里各项目的 `node_modules/**\/dist` 文件内容
  * @FilePath: /uniapp-router-view-loader/scripts/localTest.js
  */
@@ -20,11 +20,19 @@ console.log(chalk.green(`Tip: 测试项目本地部署测试文件开始`));
 for (const childPath of dirChildrenPathAll) {
     const toDir = './' + path.join(dirPath, childPath, 'node_modules', name, 'dist')
 
+    // 判断 `./examples/xxx` 文件目录
+    if (!fs.lstatSync('./' + path.join(dirPath, childPath)).isDirectory()) {
+        continue;
+    }
+
+    // 判断 `./examples/xxx/node_modules/uniapp-router-view-loader/dist` 文件目录
+    if (!fs.lstatSync(toDir).isDirectory()) {
+        continue;
+    }
+
     try {
-        if (fs.lstatSync(toDir).isDirectory()) {
-            fse.copySync('./dist', toDir)
-            console.log(chalk.yellow(`Tip: 测试项目本地部署成功【${toDir}】`));
-        }
+        fse.copySync('./dist', toDir)
+        console.log(chalk.yellow(`Tip: 测试项目本地部署成功【${toDir}】`));
     } catch (err) {
         console.error(err)
     }
